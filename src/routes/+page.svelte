c63a604973cd6d37ab87e9b2ee8e2c159cb84b69
rohanrajpal <rohan46000@gmail.com>
2023-09-19 12:16:19 +0530

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Combobox from './Combobox.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Combobox defaultSelected={['Kentaro Miura', 'Junji Ito']} />
